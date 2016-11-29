f082fdad9aa4165122a06ff70d2e9c1856823d9c
Joshua Bechard <josh@trashmountain.com>
2016-11-29 09:32:01 -0600

    Change the update funds method to only pull non-expired funds

--- server/methods.js
+++ server/methods.js
@@ -101,7 +101,7 @@ Meteor.methods({
       this.unblock();
       let splitData;
       if(!fundId){
-        let trips = Trips.find().map(function ( trip ) {
+        let trips = Trips.find({expires: {$gte: new Date()}}).map(function ( trip ) {
           return getDTSplitData( trip.tripId );
         });
         splitData = trips;
