3b4eb337bd1400145b71653741144bc9c5a8780c
JoshJoe <josh@joshjoe.com>
2017-06-05 09:15:18 -0500

    Fixed email send config

--- server/Mandrill.js
+++ server/Mandrill.js
@@ -1,4 +1,6 @@
 Mandrill.config( {
   username: Meteor.settings.Email.mandrillUsername,
-  "key":    Meteor.settings.Email.mandrillKey
+  "key":    Meteor.settings.Email.mandrillKey,
+  port: 587,
+  host: "smtps.mandrillapp.com"
 } );
