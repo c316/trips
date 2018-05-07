8d4c232bf8aec07a8bcc76b9b233ffffdee189fd
Josh <josh@joshjoe.com>
2018-05-07 14:16:45 -0500

    Getting trip name from admin's view of the page

--- client/TripsRegistration.js
+++ client/TripsRegistration.js
@@ -1,40 +1,50 @@
 import { statuses } from '/imports/api/miscFunctions';
 
-Template.TripRegistration.onCreated(function () {
-  this.autorun(()=>{
-    Meteor.subscribe('Trips');
+Template.TripRegistration.onCreated(function() {
+  const user = Meteor.users.findOne({ _id: Session.get('showingUserId') });
+  this.autorun(() => {
+    Meteor.subscribe('Trips', user && user.tripId);
   });
 });
 
 Template.TripRegistration.helpers({
-  status(){
-    let tripId = Meteor.users.findOne({_id: this._id}) && Meteor.users.findOne({_id: this._id}).tripId;
-    if(tripId){
+  status() {
+    const tripId =
+      Meteor.users.findOne({ _id: this._id }) &&
+      Meteor.users.findOne({ _id: this._id }).tripId;
+    if (tripId) {
       return statuses.completed;
-    } else {
-      return statuses.notStarted;
     }
+    return statuses.notStarted;
   },
-  tripName(){
-    return Trips.findOne({tripId: this.tripId}) && Trips.findOne({tripId: this.tripId}).name;
+  tripName() {
+    return (
+      Trips.findOne({ tripId: this.tripId }) &&
+      Trips.findOne({ tripId: this.tripId }).name
+    );
   },
 });
 
 Template.TripRegistration.events({
-  'click #tripRegistrationFormSubmit, submit #tripRegistrationForm'(e){
+  'click #tripRegistrationFormSubmit, submit #tripRegistrationForm'(e) {
     e.preventDefault();
-    console.log("Got here");
-    let tripId = $("[name='trip-id']").val().trim();
-    let updateThisId = FlowRouter.getQueryParam('id');
-    Meteor.call("form.tripRegistration", tripId, updateThisId, function ( err, res ) {
-      if(err) {
+    console.log('Got here');
+    const tripId = $("[name='trip-id']")
+      .val()
+      .trim();
+    const updateThisId = FlowRouter.getQueryParam('id');
+    Meteor.call('form.tripRegistration', tripId, updateThisId, function(
+      err,
+      res,
+    ) {
+      if (err) {
         console.error(err);
         Bert.alert({
           title: 'Error',
           message: err.reason,
           type: 'danger',
           style: 'growl-bottom-right',
-          icon: 'fa-thumbs-down'
+          icon: 'fa-thumbs-down',
         });
       } else {
         Bert.alert({
@@ -42,25 +52,29 @@ Template.TripRegistration.events({
           message: 'Ok, you are registered for this trip.',
           type: 'success',
           style: 'growl-bottom-right',
-          icon: 'fa-thumbs-up'
+          icon: 'fa-thumbs-up',
         });
         location.reload();
       }
     });
   },
   'click .change-trip'() {
-    Meteor.call("moveCurrentTripToOtherTrips", Session.get("showingUserId"), function (err, res) {
-      if(err) console.error(err);
-      else {
-        Bert.alert({
-          title: 'Old trip removed',
-          message: 'Ok, you can now register for a new trip.',
-          type: 'success',
-          style: 'growl-bottom-right',
-          icon: 'fa-thumbs-up'
-        });
-        location.reload();
-      }
-    });
-  }
+    Meteor.call(
+      'moveCurrentTripToOtherTrips',
+      Session.get('showingUserId'),
+      function(err, res) {
+        if (err) console.error(err);
+        else {
+          Bert.alert({
+            title: 'Old trip removed',
+            message: 'Ok, you can now register for a new trip.',
+            type: 'success',
+            style: 'growl-bottom-right',
+            icon: 'fa-thumbs-up',
+          });
+          location.reload();
+        }
+      },
+    );
+  },
 });
